9218903c57deb7384ec2a2f280b16872a0f441ed
Betsy Sallee <ensallee@gmail.com>
2018-03-02 18:33:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, character)
     display_board(board)
   else
     turn(board)
