3672cdd892b861f2edbf006dffbfb61c98c6e940
Betsy Sallee <ensallee@gmail.com>
2018-03-02 18:57:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,6 +38,7 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
+  character = "X" || character = "O"
   if valid_move?(board, index)
     move(board, index, character)
     display_board(board)
